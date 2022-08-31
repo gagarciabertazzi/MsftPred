14f63189bd022648e58b00b615e38df35a9462d5
Gabrielle Garcia <ga.garciabertazzi@gmail.com>
2022-08-31 00:23:58 -0500

    added streamlit setup.sh to workflow

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler as mini
 from sklearn.model_selection import train_test_split
-from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie
 import streamlit as st
 import requests
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+#def load_lottieurl(url: str):
+#    r = requests.get(url)
+#    if r.status_code != 200:
+#        return None
+#    return r.json()
 
-lottie_stock = load_lottieurl('https://assets4.lottiefiles.com/packages/lf20_jhu1lqdz.json')
-st_lottie(lottie_stock, speed=1, height=200, key="initial")
+#lottie_stock = load_lottieurl('https://assets4.lottiefiles.com/packages/lf20_jhu1lqdz.json')
+#st_lottie(lottie_stock, speed=1, height=200, key="initial")
 st.title('Microsoft Stock Predictor')
 
 st.write("Stock Predictor for X Days:")
