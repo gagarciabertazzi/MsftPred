24e083cf8372b87396150c60ffc6d74c06191160
Gabrielle Garcia <ga.garciabertazzi@gmail.com>
2022-08-30 21:57:03 -0500

    msft

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler as mini
+from sklearn.model_selection import train_test_split
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+model = joblib.load('msft.pkl')
+
+lottie_stock = load_lottieurl('https://assets4.lottiefiles.com/packages/lf20_jhu1lqdz.json')
+st_lottie(lottie_stock, speed=1, height=200, key="initial")
+st.title('Microsoft Stock Predictor')
+
+st.write("Stock Predictor for X Days:")
+
+st.sidebar.title('Stock')
+add_day = st.sidebar.number_input(label='Day:', min_value=1, max_value=35, step=1)
+
+data = np.array([[add_day]])
+y = model.predict(data)
